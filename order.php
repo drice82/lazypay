280cc0191ca042d875059bc7d7f7aeab0b1830a6
drice82 <xujiabin82@gmail.com>
2017-05-05 17:08:17 +0800

    Update order.php

--- order.php
+++ order.php
@@ -54,6 +54,9 @@ function checkstr(str){
         <font color="#FF0000">* 必填</font> &nbsp;&nbsp;&nbsp;作为购买凭证</td>
     </tr>
     <tr>
+      <td   height="50" colspan="3" class="td_border"><font color="#FF0000">* 付款后请勿关闭页面，将自动跳转到商品信息页面。</font></td>
+    </tr>
+    <tr>
       <td   height="50"  class="td_border">&nbsp;</td>
       <td  class="td_border">
 	  <input id="WID_no" name="WIDout_trade_no" type="hidden" value="000000000000" size="35"  />
