720d82296ba0ffcba771ef70b1a4cf5077b69eee
drice82 <xujiabin82@gmail.com>
2017-05-06 17:29:41 +0800

    Update order.php

--- order.php
+++ order.php
@@ -3,28 +3,57 @@
 <head>
 <script type="text/javascript">
 
-function dont(){
-	var input = document.getElementById('userphone');
-	if ((input.value.length) < 5) {
-		alert("长度太短！")
-		return false;
-		}
-	return true;
-	}
 
 function postData(){
-var input = document.getElementById('userphone');
-var input_out_trade_no = document.getElementById('WID_no');
-input_out_trade_no.value = checkstr(input.value);
+  var input = document.getElementById('userphone');
+  var input_out_trade_no = document.getElementById('WID_no');
+
+  var opt = document.getElementById('opt');
+  if (opt.value == "") {
+    alert("请选择型号")
+    return false;
+  }
+  if ((input.value.length) < 5) {
+    alert("输入的号码长度太短！")
+    return false;
+    }
+  input_out_trade_no.value = checkstr(input.value);
+  return true;
 }
 
 function checkstr(str){
-    str ='00000000000'+str;
+  var opt = document.getElementById('opt');
+  str ='00000000000'+str;
 	str = str.substr(str.length-11);
-	str = "SO"+String(Date.parse(new Date())/1000)+str+"99991";
-    return str;
+	str = "SO"+String(Date.parse(new Date())/1000)+str+opt.value;
+  return str;
 }
 
+
+function orderDetail(obj){
+  var txt = document.getElementById('txt');
+  var index = obj.selectedIndex;
+  switch(index){
+    case 1:
+    txt.innerHTML="OVH机房100M带宽，2T硬盘，130元／月";
+    break;
+
+    case 2:
+    txt.innerHTML="OVH机房100M带宽，2T硬盘，130元／月";
+    break;
+
+    case 3:
+    txt.innerHTML="Online机房125M带宽，1T硬盘，85元／月";
+    break;
+
+    case 4:
+    txt.innerHTML="Online机房250M带宽，2T硬盘，150元／月";
+    break;
+
+    default:
+    txt.innerHTML="请选择盒子型号";
+  }
+}
 
 </script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -44,10 +73,25 @@ function checkstr(str){
   </form>
 </table>
 <table border="0" cellpadding="0" cellspacing="0" class="tb_style">
-  <form name="shanpayment" onsubmit="return dont()" action="shanpay.php" method="post" target="_blank" >
+  <form name="shanpayment" action="shanpay.php" method="post" target="_blank" >
     <tr>
       <td height="50"  colspan="3"class="td_title"><span class="title">雷击霹雳-订单系统</span></td>
     </tr>
+    <tr>
+      <td   height="50"  class="td_border"><font color="#FF0000">* </font>选择商品：</td>
+      <td   class="td_border">
+        <select onchange="orderDetail(this)" id="opt">
+        <option value ="">--请选择--</option>
+        <option value ="99991">PT盒子-型号1</option>
+        <option value ="99992">PT盒子-型号2</option>
+        <option value ="99993">PT盒子-型号3</option>
+        <option value ="99994">PT盒子-型号4</option>
+        </select>
+
+      </td>
+      <td   height="50"  class="td_border"><font color="#FF0000">* </font><span id="txt">请选择盒子型号</span></td>
+    </tr>
+
     <tr>
       <td   height="50"  class="td_border"><font color="#FF0000">* </font>请输入手机号或QQ号：</td>
       <td colspan="2"  class="td_border"><input id="userphone" name="phone_no" type="text" value="" maxlength="11"  size="35" onkeyup="value=value.replace(/[^\d]/g,'')" onbeforepaste="clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d]/g, ''))"  />        
@@ -63,7 +107,7 @@ function checkstr(str){
 	  <input  name="WIDsubject" type="hidden" value="order1" size="35" />
 	  <input name="WIDtotal_fee" type="hidden" value="0.1" size="35" />
 	  <input name="WIDbody" type="hidden" value="收银" size="35" />
-	  <input onclick="postData()" type="submit" name="Submit" value="立即下单" class="btn_save" id="addnew"/>
+	  <input onclick="return postData()" type="submit" name="Submit" value="立即下单" class="btn_save" id="addnew"/>
       &nbsp;&nbsp;&nbsp;&nbsp;</td>
       <td  class="td_border">&nbsp;</td>
     </tr>
