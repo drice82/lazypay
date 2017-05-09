e233573789cb8fb4a17736d68aac20d146fb0c01
drice82 <xujiabin82@gmail.com>
2017-05-09 20:24:17 +0800

    Update get_back_info.php

--- get_back_info.php
+++ get_back_info.php
@@ -5,7 +5,7 @@
  */
 require_once("../shanpayconfig.php");
 require_once("lib/shanpayfunction.php");
-require_once("check_input.php");
+require_once("func.php");
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
