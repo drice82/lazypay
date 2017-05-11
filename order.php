8e29424e37ffed2e0f4cb789409df52dfc428d89
drice82 <xujiabin82@gmail.com>
2017-05-11 17:10:46 +0800

    Update order.php

--- order.php
+++ order.php
@@ -59,7 +59,9 @@ function postData(){
     }
   input_out_trade_no.value = checkstr(input.value);
   if (opt.value == "99991") { uinitfee.value = "<?PHP echo $init_fee99991; ?>"; }
-
+  if (opt.value == "99992") { uinitfee.value = "<?PHP echo $init_fee99992; ?>"; }
+  if (opt.value == "99993") { uinitfee.value = "<?PHP echo $init_fee99993; ?>"; }
+  if (opt.value == "99994") { uinitfee.value = "<?PHP echo $init_fee99994; ?>"; }	
   return true;
 }
 
