fa8fda19c1f8fe52c8a718cd009ae36eeba4ce71
drice82 <xujiabin82@gmail.com>
2017-06-05 10:59:53 +0800

    Update order.php

--- order.php
+++ order.php
@@ -120,6 +120,27 @@ function orderDetail(obj){
   }
 }
 
+        var system = {
+            win: false,
+            mac: false,
+            xll: false,
+            ipad:false
+        };
+        //检测平台
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+        system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+        //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+        if (system.win || system.mac || system.xll||system.ipad) {
+
+        } else {
+
+            window.location.href = "./mobile.php";
+        }	
+	
+	
 </script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link  href="images/style.css" rel="stylesheet" type="text/css" />
