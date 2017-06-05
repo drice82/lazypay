59a49e2c5b6d50fcf1f7a541f9314fe7c5525d82
drice82 <xujiabin82@gmail.com>
2017-06-05 10:58:10 +0800

    Add files via upload

---
+++ mobile.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript">
+
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link  href="images/style.css" rel="stylesheet" type="text/css" />
+<title>雷击霹雳-订单系统</title>
+</head><body style="background:#F3F3F4">
+<br />
+<br />
+
+<table border="0" cellpadding="0" cellspacing="0" class="tb_style">
+  <form action="shanpay.php" method="post" name="shanpayment" target="_blank" id="shanpayment">
+    <tr>
+      <td width="210"   height="50"  class="td_border"><a href="http://www.lazypt.net"><img src="images/logo.png" border="0"></a></td>
+      <td width="412"  class="td_border">&nbsp;</td>
+      <td width="178"  class="td_border"><strong><a href="http://www.lazypt.net/pay/get_back.php">过往订单查询</a></strong></td>
+    </tr>
+  </form>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="tb_style">
+  <form name="shanpayment" action="shanpay.php" method="post" target="_blank" >
+    <tr>
+      <td height="50"  colspan="3" class="td_title"><span class="title">雷击霹雳-订单系统</span></td>
+    </tr>
+    <tr>
+	    <td   height="50" colspan="3"  class="td_border"><font color="#FF0000">*本页面不支持手机端，请通过电脑访问。 </font>
+	    </td>
+    </tr>
+    <tr>
+
+      </td>
+    </tr>
+
+    <tr>
+    </tr>
+    <tr>
+    </tr>
+    <tr>
+      <td   height="50"  class="td_border">&nbsp;</td>
+      <td  class="td_border">
+      <td  class="td_border">&nbsp;</td>
+    </tr>
+  </form>
+</table>
+
+<div class="bottom">
+ Powered BY <a href="http://www.lazypt.net" target="_blank">雷击霹雳</a>
+ &copy; 2015-2017&nbsp;&nbsp;<a href="http://www.lazypt.net" target="_blank">LazyPT Inc.</a>
+  
+</div>
+</body>
+</html>
